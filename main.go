7b1bd5d3cafbd920ec855a372804cb65a62f0d84
Kevin <itsksrof@proton.me>
2023-10-17 14:42:19 +0200

    Switch from SDL2 to Fyne

--- main.go
+++ main.go
@@ -1,45 +1,29 @@
 package main
 
 import (
-	"log"
+	"time"
 
-	"github.com/veandco/go-sdl2/sdl"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
-const SCREEN_WIDTH int32 = 640
-const SCREEN_HEIGHT int32 = 400
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}
 
 func main() {
-	var window *sdl.Window = nil
-	var surface *sdl.Surface = nil
-	var err error
+	a := app.New()
+	w := a.NewWindow("Hello World")
 
-	defer sdl.Quit()
-	defer window.Destroy()
+	clock := widget.NewLabel("")
+	updateTime(clock)
 
-	if sdl.INIT_VIDEO < 0 {
-		log.Fatalf("SDL could not initialize! SDL_Error: %v\n", sdl.GetError())
-	} else {
-		window, err = sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, sdl.WINDOW_SHOWN)
-		if err != nil {
-			log.Fatalf("Window could not be created! SDL_Error: %v\n", sdl.GetError())
+	w.SetContent(clock)
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
 		}
-
-		surface, err = window.GetSurface()
-		if err != nil {
-			log.Fatalf("Surface could not be found! SDL_Error: %v\n", sdl.GetError())
-		}
-
-		surface.FillRect(nil, sdl.MapRGB(surface.Format, 0xFF, 0xFF, 0xFF))
-		window.UpdateSurface()
-
-		var quit bool = false
-		for !quit {
-			for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-				if event.GetType() == sdl.QUIT {
-					quit = true
-				}
-			}
-		}
-	}
+	}()
+	w.ShowAndRun()
 }
