1db99dc1126deaf5876acf0ddf74a871902e613d
Kevin <ksrof@proton.me>
2023-10-18 14:31:15 +0200

    Generate and run temp .go file, update label with output

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
+type ctxKey struct{}
+
 type KeyableEntry struct {
+	context.Context
 	widget.Entry
 }
 
-func NewKeyableEntry() *KeyableEntry {
-	entry := &KeyableEntry{}
+func NewKeyableEntry(ctx context.Context) *KeyableEntry {
+	entry := &KeyableEntry{Context: ctx}
 	entry.MultiLine = true
 	entry.ExtendBaseWidget(entry)
 	return entry
@@ -30,7 +39,28 @@ func (e *KeyableEntry) TypedShortcut(shortcut fyne.Shortcut) {
 	}
 
 	if cs.Key() == fyne.KeyReturn && cs.Mod() == fyne.KeyModifierControl {
-		log.Println(e.Text)
+		timestamp := time.Now().Unix()
+
+		if err := os.WriteFile(
+			fmt.Sprintf("%d.go", timestamp),
+			[]byte(e.Text),
+			0644,
+		); err != nil {
+			log.Fatal(err)
+		}
+
+		out, err := exec.Command("go", "run", fmt.Sprintf("%d.go", timestamp)).CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := os.Remove(fmt.Sprintf("%d.go", timestamp)); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("[DEBUG]", out)
+		fmt.Println("[DEBUG]", string(out))
+		e.Context = context.WithValue(e.Context, ctxKey{}, string(out))
 	}
 }
 
@@ -38,14 +68,26 @@ func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("RunGo")
 
-	entry := NewKeyableEntry()
-	label := widget.NewLabel("Type some code to start!")
+	entry := NewKeyableEntry(context.Background())
+	str := binding.NewString()
+	str.Set("Type some code to start!")
+	label := widget.NewLabelWithData(str)
 
 	ctrlReturn := &desktop.CustomShortcut{KeyName: fyne.KeyReturn, Modifier: fyne.KeyModifierControl}
 	myWindow.Canvas().AddShortcut(ctrlReturn, entry.Entry.TypedShortcut)
 
 	grid := container.New(layout.NewGridLayout(2), entry, label)
 	myWindow.Canvas().SetContent(grid)
+
+	go func() {
+		for range time.Tick(time.Millisecond * 100) {
+			if entry.Context.Value(ctxKey{}) == nil {
+				continue
+			} else {
+				str.Set(entry.Context.Value(ctxKey{}).(string))
+			}
+		}
+	}()
 
 	myWindow.Resize(fyne.NewSize(1024, 640))
 	myWindow.ShowAndRun()
