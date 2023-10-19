32354439c1ebedb7980a3016ef9c5bf9c0345ff7
Kevin <ksrof@proton.me>
2023-10-19 13:25:09 +0200

    events: Add event to create a temp .go file and run it

---
+++ events/run.go
@@ -0,0 +1,23 @@
+package events
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func CreateTempAndRun(data []byte) (string, error) {
+	filename := fmt.Sprintf("%d.go", time.Now().Unix())
+	if err := os.WriteFile(filename, data, 0644); err != nil {
+		return "", err
+	}
+
+	out, _ := exec.Command("go", "run", filename).CombinedOutput()
+
+	if err := os.Remove(filename); err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
