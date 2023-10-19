2d7faaa9502e6cf00a2c2c5731d119d2697f137e
Kevin <ksrof@proton.me>
2023-10-19 13:24:39 +0200

    widgets: Add data binded console widget

---
+++ widgets/console.go
@@ -0,0 +1,18 @@
+package widgets
+
+import (
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Console struct {
+	widget.Label
+}
+
+func NewConsole(str binding.String) *Console {
+	console := &Console{}
+	console.Bind(str)
+	console.ExtendBaseWidget(console)
+
+	return console
+}
