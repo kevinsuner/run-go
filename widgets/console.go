d96d990f72f535de81a13560ed30bc8ad48213fa
Kevin <ksrof@proton.me>
2023-10-23 17:49:53 +0200

    widgets: Set wrapping for label to wrap-break

--- widgets/console.go
+++ widgets/console.go
@@ -2,6 +2,7 @@
 package widgets
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
@@ -12,6 +13,7 @@ type Console struct {
 
 func NewConsole(output binding.String) *Console {
 	console := &Console{}
+	console.Label.Wrapping = fyne.TextWrapBreak
 	console.Bind(output)
 	console.ExtendBaseWidget(console)
 
