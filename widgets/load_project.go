c091318f133fc91ad0035985536e31bc69289d00
Kevin <ksrof@proton.me>
2023-11-27 11:53:51 +0100

    widgets: Add button to close modal popup

--- widgets/load_project.go
+++ widgets/load_project.go
@@ -58,37 +58,43 @@ func (l *LoadProjectPopUp) TypedShortcut(shortcut fyne.Shortcut) {
 			os.Exit(1)
 		}
 
-		l.PopUp = widget.NewModalPopUp(widget.NewList(
-			func() int {
-				return len(projects)
-			},
-			func() fyne.CanvasObject {
-				return widget.NewButton("template", nil)
-			},
-			func(id widget.ListItemID, obj fyne.CanvasObject) {
-				button := obj.(*widget.Button)
-				button.SetText(projects[id])
-				button.OnTapped = func() {
-					if l.input.Entry.Text == "" {
-						data, err := events.LoadGoProject(button.Text)
-						if err != nil {
-							fmt.Fprintln(os.Stderr, err)
-							os.Exit(1)
+		l.PopUp = widget.NewModalPopUp(container.NewGridWithRows(2,
+			widget.NewList(
+				func() int {
+					return len(projects)
+				},
+				func() fyne.CanvasObject {
+					return widget.NewButton("template", nil)
+				},
+				func(id widget.ListItemID, obj fyne.CanvasObject) {
+					loadProjectButton := obj.(*widget.Button)
+					loadProjectButton.SetText(projects[id])
+					loadProjectButton.OnTapped = func() {
+						if l.input.Entry.Text == "" {
+							data, err := events.LoadGoProject(loadProjectButton.Text)
+							if err != nil {
+								fmt.Fprintln(os.Stderr, err)
+								os.Exit(1)
+							}
+
+							if err := l.projectName.Set(loadProjectButton.Text); err != nil {
+								fmt.Fprintln(os.Stderr, err)
+								os.Exit(1)
+							}
+
+							l.tabs.Selected().Text = loadProjectButton.Text
+							l.tabs.Refresh()
+							l.input.Entry.SetText(data)
 						}
 
-						if err := l.projectName.Set(button.Text); err != nil {
-							fmt.Fprintln(os.Stderr, err)
-							os.Exit(1)
-						}
-
-						l.tabs.Selected().Text = button.Text
-						l.tabs.Refresh()
-						l.input.Entry.SetText(data)
+						l.PopUp.Hide()
 					}
-
-					l.PopUp.Hide()
-				}
-			},
+				},
+			),
+			widget.NewButton("Close", func() {
+				fmt.Fprintln(os.Stdout, "closeModalButton clicked")
+				l.PopUp.Hide()
+			}),
 		), l.canvas)
 
 		l.PopUp.Resize(fyne.NewSize(1024, 640))
